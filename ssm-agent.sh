15e0b22dc828401b6dc41c2800da0638184b894f
yukin01 <38382781+yukin01@users.noreply.github.com>
2022-03-09 15:43:22 +0900

    feat(all): 踏み台起動時にタグを付与する

--- ssm-agent.sh
+++ ssm-agent.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
 if [ -z "${BASTION_ROLE_NAME}" ] || [ -z "${BASTION_INSTANCE_NAME}" ]; then
-    echo "ERROR: Some variables are not found. Please set BASTION_ROLE_NAME and BASTION_INSTANCE_NAME." >&2
+    echo "[ERROR] Some variables are not found. Please set BASTION_ROLE_NAME and BASTION_INSTANCE_NAME." >&2
     exit 1
 fi
 
-function activate(){
-    echo "start activate process ..."
-    activation=$(aws ssm create-activation --default-instance-name "${BASTION_INSTANCE_NAME}" --iam-role "${BASTION_ROLE_NAME}" --output text)
-    SSM_AGENT_CODE=$(echo $activation | cut -f 1 -d ' ')
-    SSM_AGENT_ID=$(echo $activation | cut -f 2 -d ' ')
+function activate() {
+    echo "[INFO] start activate process ..."
+    local activation=$(aws ssm create-activation --default-instance-name "${BASTION_INSTANCE_NAME}" --iam-role "${BASTION_ROLE_NAME}" --output text)
+    local activation_code=$(echo "$activation" | cut -f 1 -d ' ')
+    local activation_id=$(echo "$activation" | cut -f 2 -d ' ')
+    amazon-ssm-agent -register -code "$activation_code" -id "$activation_id" -region "ap-northeast-1"
+    aws ssm delete-activation --activation-id "$activation_id"
+    echo "[INFO] activate process completed."
 }
 
-function shutdown(){
-    echo "start shutdown process ..."
+function shutdown() {
+    echo "[INFO] start shutdown process ..."
     instance_id=$(cat /var/lib/amazon/ssm/registration | jq -r .ManagedInstanceID)
-    aws ssm deregister-managed-instance --instance-id $instance_id
+    aws ssm deregister-managed-instance --instance-id "$instance_id"
     kill $(pgrep amazon-ssm)
-    echo "shutdown process completed."
+    echo "[INFO] shutdown process completed."
 }
 
-function start(){
-    amazon-ssm-agent -register -code "${SSM_AGENT_CODE}" -id "${SSM_AGENT_ID}" -region "ap-northeast-1"
-    aws ssm delete-activation --activation-id "${SSM_AGENT_ID}"
+function start() {
+    local product=${DD_SERVICE:none}
+    local env=${DD_ENV:none}
+    instance_id=$(cat /var/lib/amazon/ssm/registration | jq -r .ManagedInstanceID)
+    aws ssm add-tags-to-resource --resource-type "ManagedInstance" --resource-id "$instance_id" --tags "Key=Product,Value=$product" "Key=Env,Value=$env"
     amazon-ssm-agent start &
-    echo "activate process completed. ssm-agent has started."
+    echo "[INFO] ssm-agent has started."
 }
 
 ## main process
